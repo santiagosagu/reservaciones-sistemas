a30498bb325386ff533f8795fbbc64b83d021fbd
santiagosagu <danielsantiagono10@gmail.com>
2024-10-11 13:53:37 -0500

    remove pusher

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,8 @@ import "./App.css";
 import useRouterApp from "./routes";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
 function App() {
-  const beamsClient = new PusherPushNotifications.Client({
-    instanceId: "aeb4dd00-593d-433e-babe-659cfab9ba0f",
-  });
-
-  beamsClient
-    .start()
-    .then(() => beamsClient.addDeviceInterest("hello"))
-    .then(() => console.log("Successfully registered and subscribed!"))
-    .catch(console.error);
-
   const queryClient = new QueryClient();
 
   return (
